bb18386a20d5296507c85471eb110f752d04c741
t-88 <temptaki5002@gmail.com>
2024-07-31 08:13:48 +0100

    AA

--- App.java
+++ App.java
@@ -14,6 +14,7 @@ class Dexer {
 
   }
 }
+
 class A {
   public void B() {
     try {
--- Dex__.java
+++ Dex__.java
@@ -1,5 +1,5 @@
 
-public class Dex {
+public class Dex__ {
     public static  void main(String[] args) {
         System.out.println("Chaning from github");
     }
